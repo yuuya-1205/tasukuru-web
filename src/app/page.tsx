fe78249e76a2b969c33c12ffdc6062d048bfde7d
ushi <>
2026-09-06 16:26:41 +0900

    feature: demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,121 +1,17 @@
-'use client'
+import type { Metadata } from 'next'
 
-import Image from 'next/image'
-import { useState } from 'react'
-import heroImg from '../assets/hero.png'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/vite.svg'
+export const metadata: Metadata = {
+  title: '最初のページ',
+  description: 'playwrightハンズオンの最初のステップ',
+}
 
 export default function Home() {
-  const [count, setCount] = useState(0)
+  return <main>
+    <h1>playwrightハンズオン</h1>
+    <p>あなたは1週間後にはE2Eテストを書けるようになります</p>
+    <p>
+      <button>操作ボタン</button>
+    </p>
 
-  return (
-    <main id="root">
-      <section id="center">
-        <div className="hero">
-          <Image
-            src={heroImg}
-            className="base"
-            width="170"
-            height="179"
-            alt=""
-            priority
-          />
-          <Image
-            src={reactLogo}
-            className="framework"
-            alt="React logo"
-          />
-          <Image src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/app/page.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((currentCount) => currentCount + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://nextjs.org/docs" target="_blank">
-                <Image className="logo" src={viteLogo} alt="" />
-                Explore Next.js
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <Image className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vercel/next.js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://nextjs.org/discord" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/nextjs" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </main>
-  )
+  </main>
 }
